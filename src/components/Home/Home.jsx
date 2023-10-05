fca24827fac7b72bf5fca835849a3de617ef4b9f
Md. Hasanur Rahman <hasanurhasu127@gmail.com>
2023-10-05 21:05:17 +0600

    create CreateUser Function

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,11 @@
+import { useContext } from "react";
+import { AuthContext } from "../AuthContext/AuthProvider";
 
 const Home = () => {
+    const authInfo = useContext(AuthContext);
     return (
         <div>
-            <h1 className="text-2xl text-center font-bold">This is home</h1>
+            <h1 className="text-2xl text-center font-bold">{authInfo.name}</h1>
         </div>
     );
 };
